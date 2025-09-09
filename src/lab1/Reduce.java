35a4caf4264094fd25ec0b840735b6be6cd502f5
mayyadow <mayadowman2024@gmail.com>
2025-09-09 12:50:14 -0400

    implemented reduce method

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class Reduce{
+    static void main() {
+    }
+
+    public static int reduce(int n){
+        int count = 0;
+        while( n!= 0){
+            if(n%2 == 0){
+                n = n/2;
+            }
+            else{
+                n = n - 1;
+            }
+            count++;
+        }
+        return count;
+    }
+
+
+}
